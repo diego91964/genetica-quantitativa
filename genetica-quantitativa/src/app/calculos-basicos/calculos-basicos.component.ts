f94e80fe252dc746fd7cbf8e884958bf9f3376fc
Diego Silva <diego.silva.facom@gmail.com>
2017-09-26 16:05:05 -0300

    Adicionando inicio de calculo de media de individuos

--- genetica-quantitativa/src/app/calculos-basicos/calculos-basicos.component.ts
+++ genetica-quantitativa/src/app/calculos-basicos/calculos-basicos.component.ts
@@ -9,9 +9,9 @@ import { FormsModule } from '@angular/forms';
 export class CalculosBasicosComponent implements OnInit {
 
   //paiUm   = [];
-  paiDois = [];
-  fUm     = [];
-  fDois   = [];
+  //paiDois = [];
+  //fUm     = [];
+  //fDois   = [];
 
   paiUmString: string = '';
   paiDoisString: string = '';
@@ -35,15 +35,56 @@ export class CalculosBasicosComponent implements OnInit {
 
   calcular (){
 
-     let paiUm = new Individuo(this.normalizaLista(this.paiUmString));
+     let paiUm   = new Individuo(this.normalizaLista(this.paiUmString));
+     let paiDois = new Individuo(this.normalizaLista(this.paiDoisString));
+     let fUm     = new Individuo(this.normalizaLista(this.fUmString));
+     let fDois   = new Individuo(this.normalizaLista(this.fDoisString));
+
+     let variancaPaiUm =  paiUm.calculoVariancaFenotipica(3);
+     let variancaPaiDois = paiDois.calculoVariancaFenotipica(3);
+     let variancaFUm = fUm.calculoVariancaFenotipica(3);
 
      alert('Media: ' + paiUm.calculoMediaFenotipica(3));
-     alert('Variancca: ' + paiUm.calculoVariancaFenotipica(3));
+     alert('Variança: ' + variancaPaiUm);
 
+     let variancaAmbiental = this.calculoVariancaAmbiental(variancaPaiUm,variancaPaiDois,variancaFUm);
+     let variancaGeneticaF2 = this.calculoVariancaGenetica(variancaPaiUm,variancaAmbiental);
+
+     alert('Variança genética F2 ' + variancaGeneticaF2);
+     //alert('Herdabilidade ' + this.calculoDaHerdabilidade());
   }
 
-  calculoVariancaAmbiental () {
-    
+  calculoVariancaAmbiental (vPaiUm, vPaiDois, vFum) {
+    return (vPaiUm + vPaiDois + 2*vFum) / 4;
+  }
+
+  calculoVariancaGenetica (vFdois, vAmbiental) {
+    return (vFdois - vAmbiental);
+  }
+
+  calculoDaHerdabilidade (vGeneticaF2, vFenotipicaF2) {
+    return (vGeneticaF2/vFenotipicaF2);
+  }
+
+  calculoNumeroDeGenes (mediaP1, mediaP2, variancaGeneticaF2){
+    return Math.pow((mediaP1 - mediaP2), 2) / (8*variancaGeneticaF2);
+  }
+
+  calculoGanhoDeSelecao (herdabilidade, mediaF2, listaIndividuos: any[], percentualSelecao ){
+
+    let individuosSelecionados = this.selecionaIndividuosDoVetor(listaIndividuos,percentualSelecao);
+    let somaIndividuosSelecionados:number   = individuosSelecionados.reduce((prev,next)=>prev+(+next),0);
+    let mediaIndividuosSelecionados:number   = somaIndividuosSelecionados / individuosSelecionados.length;
+
+
+    return herdabilidade*(mediaF2 - mediaIndividuosSelecionados);
+  }
+
+  selecionaIndividuosDoVetor (individuos: any [] , porcentagemSelecionados: number ){
+    let individuosSorted = individuos.sort();
+    let tamanhoLista: number = individuosSorted.length;
+    let indiceSelecionado: number = parseInt((tamanhoLista*(porcentagemSelecionados/100)) + '');
+    return individuos.splice(indiceSelecionado,(tamanhoLista-indiceSelecionado));
   }
 
   normalizaLista (listaString:string){
