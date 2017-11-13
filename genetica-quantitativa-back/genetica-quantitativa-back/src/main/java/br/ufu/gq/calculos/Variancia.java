8945e15191da0c44588258987f7168e30afa8eea
Diego A. Silva <diego.silva.facom@gmail.com>
2017-11-13 15:23:39 -0200

    Adicionando templante de teste

--- genetica-quantitativa-back/genetica-quantitativa-back/src/main/java/br/ufu/gq/calculos/Variancia.java
+++ genetica-quantitativa-back/genetica-quantitativa-back/src/main/java/br/ufu/gq/calculos/Variancia.java
@@ -1,5 +1,55 @@
 package br.ufu.gq.calculos;
 
+import java.math.BigDecimal;
+
 public class Variancia {
 
+	public static BigDecimal calculoDeCorrecoes(BigDecimal dados[][], Integer gr) {
+
+		BigDecimal soma = new BigDecimal(0);
+
+		for (BigDecimal linha[] : dados) {
+
+			for (BigDecimal dado : linha) {
+				soma.add(dado);
+			}
+		}
+
+		return soma.divide(new BigDecimal(gr));
+	}
+
+	public static BigDecimal calculoSomaDeQuadrado(BigDecimal dados[][], BigDecimal correcao) {
+
+		BigDecimal soma = new BigDecimal(0);
+		for (BigDecimal linha[] : dados) {
+
+			for (BigDecimal dado : linha) {
+				soma.add(dado.multiply(dado));
+			}
+		}
+		
+		return soma.subtract(correcao);
+	}
+
+	public static BigDecimal somaLinha (BigDecimal dados[][], Integer coluna){
+		
+		BigDecimal soma = new BigDecimal(0);
+		
+		for (int i = 0 ; i < dados.length; i++){
+			soma.add(dados[i][coluna]);
+		}
+		
+		return soma;
+	}
+	
+	public static BigDecimal somaColuna (BigDecimal dados[][], Integer coluna){
+		
+		BigDecimal soma = new BigDecimal(0);
+		
+		for (int i = 0 ; i < dados.length; i++){
+			soma.add(dados[i][coluna]);
+		}
+		
+		return soma;
+	}
 }
